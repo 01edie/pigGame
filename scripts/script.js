1e7146c5a8facc6d69819128821ed8b26195243a
01edie <01bbose@gmail.com>
2022-04-29 11:00:17 +0530

    with rules and player name inputs

--- scripts/script.js
+++ scripts/script.js
@@ -53,7 +53,7 @@ const holdDice = () => {
     document.querySelector(`.player-${selectedPlayer+1}`).classList.remove('active');
 
     document.querySelector('.winner-text').classList.remove('hidden');
-    document.querySelector('.winner-text').textContent = `👾player-${selectedPlayer+1} wins!`;
+    document.querySelector('.winner-text').textContent = '👾 ' + document.querySelector(`#p${selectedPlayer+1}`).innerText + ' wins!';
 
     rollButton.onclick = null;
   }
@@ -94,6 +94,33 @@ const init =()=>{
   rollButton.onclick = rollDice;
 }
 
+//rules and 1st phase
+function closeModal(){
+  document.querySelector('.rules-container').classList.add('hidden');
+  document.querySelector('.game-container').classList.remove('hidden');
+  console.log('hi');
+}
+function play(){
+  let p1_name = document.querySelector('.player-1-name').value;
+  let p2_name = document.querySelector('.player-2-name').value;
+  let errorMessage = '';
+  let t1 = true, t2 = true;
+  if(p1_name.trim() === ''){
+    errorMessage = "player-1 can't be empty! ";
+    t1 = false;
+  }
+  if(p2_name.trim() === ''){
+    errorMessage = "player-2 can't be empty! ";
+    t2 = false;
+  }
+  if(t1 && t2){
+    document.getElementById('p1').innerText = p1_name;
+    document.getElementById('p2').innerText = p2_name;
+    closeModal();
+  }else{
+    document.querySelector('.error-message').innerText = errorMessage;
+  }
+}
 init();
 //events
 
@@ -101,3 +128,5 @@ init();
 rollButton.onclick = rollDice;
 holdButton.onclick = holdDice;
 newGameButton.onclick = init;
+
+//rules and 1st phase
